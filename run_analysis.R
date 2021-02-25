57277fd2b8a216f19b3bfdf5276bccf930d87102
Prabhsimran Singh <psimran.singh@gmail.com>
2021-02-25 12:31:01 -0500

    Final Version -Added Step Descriptions -Summarised data and created final table

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,8 @@
 #Setup
 library(dplyr)
+library(reshape2)
 
+#Get Data from the Internet and Unzip
 filename <- "Datasets.zip"
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
@@ -12,6 +14,7 @@ if (!file.exists("UCI HAR Dataset")) {
   unzip(filename) 
 }
 
+#Read all tables into R
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -22,26 +25,24 @@ x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
 
-# str(features) 
-# str(activities)
-# str(subject_test)
-# str(x_test)
-# str(y_test)
-# str(subject_train)
-# str(x_train)
-# str(y_train)
-
+#Merge the Datasets
 x <- rbind(x_test,x_train)
 y <- rbind(y_test,y_train)
 subject <- rbind(subject_test,subject_train)
 
 test_train_merged <- cbind(subject,x,y)
 
+
+#Keep only mean and std variables
 tidydata <- test_train_merged %>% 
   select(subject,code,contains("mean"),contains("std"))
 
-# str(tidydata)
 
+#Name the activities explicitly rather than use codes
+tidydata$code <- activities[tidydata$code, 2]
+
+
+#Rename all variables to be descriptive
 names(tidydata)[1] = "Subject"
 names(tidydata)[2] = "Activity"
 names(tidydata)<-gsub("Acc", "Accelerometer", names(tidydata))
@@ -51,9 +52,15 @@ names(tidydata)<-gsub("Mag", "Magnitude", names(tidydata))
 names(tidydata)<-gsub("^t", "Time", names(tidydata))
 names(tidydata)<-gsub("^f", "Frequency", names(tidydata))
 names(tidydata)<-gsub("tBody", "TimeBody", names(tidydata))
-names(tidydata)<-gsub("-mean()", "Mean", names(tidydata), ignore.case = TRUE)
-names(tidydata)<-gsub("-std()", "STD", names(tidydata), ignore.case = TRUE)
-names(tidydata)<-gsub("-freq()", "Frequency", names(tidydata), ignore.case = TRUE)
+names(tidydata)<-gsub("-mean()", "Mean", names(tidydata), ignore.case = T)
+names(tidydata)<-gsub("-std()", "STD", names(tidydata), ignore.case = T)
+names(tidydata)<-gsub("-freq()", "Frequency", names(tidydata), ignore.case = T)
 names(tidydata)<-gsub("angle", "Angle", names(tidydata))
 names(tidydata)<-gsub("gravity", "Gravity", names(tidydata))
 
+
+#Create final data set with summary data at the subject and activity level
+melteddata <- melt(tidydata, id = c("Subject", "Activity"))
+finaldata <- dcast(melteddata, Subject + Activity ~ variable, mean)
+
+write.table(finaldata, "./final_tidy_dataset.txt", row.names = FALSE, quote = FALSE)
